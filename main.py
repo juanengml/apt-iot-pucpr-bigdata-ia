d72557b80c7973a8aadeace63db0b81e2b93a0b8
juanmanoel <37448831+juanengml@users.noreply.github.com>
2020-08-30 21:41:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,84 @@
 import machine
-#from datetime import datetime as dt
+from dht import DHT11
 from time import sleep
 import utime as dt 
 from random import choice 
-
-r = machine.Pin(2,machine.Pin.OUT)
+import network
+import urequests
+import ujson
 
 def controleLed(status):
     now = dt.time()
     tm = dt.localtime(now)
-    if status ==1:
-        print(str(tm),"STATUS: LED ON")
+    msg = None
+    if status == 0:
+        msg = "STATUS: LED ON"
         r.value(status)
     else:
-        print(str(tm),"STATUS: LED OFF")
+        msg = "STATUS: LED OFF"
         r.value(status)
+    return msg
 
-while True:
-     status = choice([1,0])
-     controleLed(status)
-     sleep(1)
+def SensorAmbiente():
+    for p in range(1,3):
+      sleep(1)
+    d.measure()
+    umidade = d.humidity()
+    temperatura = d.temperature()   
+    return [umidade,temperatura]
+
+def connect_wifi(ssid, password):
+    station = network.WLAN(network.STA_IF)
+    station.active(True)
+    station.connect(ssid,password)
+    for tentativa in range(50):
+        if station.isconnected():
+            break
+        sleep(0.1)
+    return station    
+
+def GenerateID():
+  reponse = None
+  try:
+    response = urequests.get("http://TintedNimbleCompilerbug--five-nine.repl.co/api/iot/v1/GENERATE_ID")
+    return response.json()
+  except:
+    return {"ID":"","dt":""} 
+
+def EnviarDadosSensores(data):
+      response = None
+      response = urequests.get("http://TintedNimbleCompilerbug--five-nine.repl.co/api/iot/v1/SEND?umidade={}&temperatura={}&relay_flag={}".format(
+                  data['umidade'],
+                  data['temperatura'],
+                  data['relay_flag'])).json()
+      return response
+       
+      
+def main():
+      station = connect_wifi("USSID","PASSWD")
+      response = GenerateID()
+      print(response)
+      while station.isconnected():
+         umidade, temperatura = SensorAmbiente()
+         
+         status = None
+         if temperatura > 31 and umidade > 70:
+             status = 0
+         else:    
+             status = 1
+             
+         value = {"umidade":str(umidade),"temperatura":str(temperatura),"relay_flag":str(status)}
+         #print("value: ", value)
+         result = EnviarDadosSensores(value)
+         print("result: ",result)
+         print("Umidade: {}\tTemperatura: {} \tSTATUS_LED: {}".format(umidade, temperatura,controleLed(status)))
+         sleep(1)
+         
+          
+if __name__== "__main__":   
+    r = machine.Pin(33,machine.Pin.OUT)
+    d = DHT11(machine.Pin(32))
+    main()
+    
 
 
